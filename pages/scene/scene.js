289eba6fb5a92b8848e52766122a407d5f4b5ab8
chenjiaxi <chenjiaxi_byr@163.com>
2018-10-18 15:12:07 +0800

    添加场景页

--- pages/scene/scene.js
+++ pages/scene/scene.js
@@ -12,7 +12,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    sceneGroup:['分组','场景'],
+    
     
   },
 
@@ -20,34 +20,13 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    //this._loadAllDevice();
-    var name = this.data.sceneGroup[0]; 
-    this._loadBaannerTitle(name);
+   
   },
 
-  onTabAddGroup: function (event){
+ 
 
-  },
-
-  /*加载本地banner和title */
-  _loadBaannerTitle: function (name) {
-
-    this.setData({
-      bannerTitle: name
-    });
-  },
-
-  onTabCategory: function (event) {
-    var index = Number(scene.getDataSet(event, 'index'));  //number化
-    var id = Number(index);    //此ID为临时ID，要根据真实DB改
-    var name = scene.getDataSet(event, 'name');
-    this.setData({
-      currentTabsIndex: index
-    });
-    this._loadBaannerTitle(name);   //加载本地banner和标题
-    //this._loadCateDevices(id, index);  //点击时获取数据
-
-  },
+  
+ 
 
 
   
--- pages/scene/scene_model.js
+++ pages/scene/scene_model.js
@@ -5,16 +5,6 @@ class Scene extends Base {
     super();
   }
 
-  getAllDevices(callback) {
-    var param = {
-      url: 'deviceaccess/tenant/devices/2?limit=1000',
-      sCallback: function (data) {
-        callback && callback(data);
-      }
-    };
-    this.request(param);
-  }
-
 }
 
 export { Scene };
