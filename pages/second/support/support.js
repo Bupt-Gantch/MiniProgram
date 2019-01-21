678f796105a65b792c79cf09b292693aeb86ad36
SongOf <46475785+SongOf@users.noreply.github.com>
2019-01-21 16:49:33 +0800

    Update support.js

--- pages/second/support/support.js
+++ pages/second/support/support.js
@@ -1,66 +1,82 @@
 // pages/second/support/support.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    category: [
+      { name: '真伪查询', id: 'check' },
+      { name: '维修查询', id: 'fix-info' },
+      { name: '查询支付', id: 'payment-info' },
+      { name: '寄送快修', id: 'packing-fix' },
+      { name: '上门快修', id: 'repair-at-home' },
+      { name: '预约维修', id: 'book-repair' },
+      { name: '其他', id: 'others' }
+    ],
+    detail: [
+      {
+        "id": "check",
+        "banner": "/imgs/swiper/swiper-03.jpg",
+        "cate": "真伪查询",
+        "detail": []
+      },
+      {
+        "id": "fix-info",
+        "banner": "/imgs/swiper/swiper-03.jpg",
+        "cate": "维修查询",
+        "detail": []
+      },
+      {
+        "id": "payment-info",
+        "banner": "/imgs/swiper/swiper-03.jpg",
+        "cate": "查询支付",
+        "detail": []
+      },
+      {
+        "id": "packing-fix",
+        "banner": "/imgs/swiper/swiper-03.jpg",
+        "cate": "寄送快修",
+        "detail": []
+      },
+      {
+        "id": "repair-at-home",
+        "banner": "/imgs/swiper/swiper-03.jpg",
+        "cate": "上门快修",
+        "detail": []
+      },
+      {
+        "id": "book-repair",
+        "banner": "/imgs/swiper/swiper-03.jpg",
+        "cate": "预约维修",
+        "detail": []
+      },
+      {
+        "id": "others",
+        "banner": "/imgs/swiper/swiper-03.jpg",
+        "cate": "其他",
+        "detail": []
+      },
+    ],
+    curIndex: 0,
+    isScroll: false,
+    toView: 'check'
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onReady() {
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  switchTab(e) {
+    const self = this;
+    this.setData({
+      isScroll: true
+    })
+    setTimeout(function () {
+      self.setData({
+        toView: e.target.dataset.id,
+        curIndex: e.target.dataset.index
+      })
+    }, 0)
+    setTimeout(function () {
+      self.setData({
+        isScroll: false
+      })
+    }, 1)
 
   }
+
 })
