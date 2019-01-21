eaecd42099443342b4cfda266369d758f07e86d3
SongOf <46475785+SongOf@users.noreply.github.com>
2019-01-21 16:43:00 +0800

    Update office.js

--- pages/second/office/office.js
+++ pages/second/office/office.js
@@ -1,66 +1,77 @@
 // pages/second/office/office.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    category: [
+      { name: '管理人员', id: 'exc' },
+      { name: '人事部', id: 'hr' },
+      { name: '销售部', id: 'sale-club' },
+      { name: '研发部', id: 'study-club' },
+      { name: '新员工', id: 'newWorker' },
+      { name: '其他', id: 'others' }
+    ],
+    detail: [
+      {
+        "id": "exc",
+        "banner": "/imgs/swiper/swiper-03.jpg",
+        "cate": "管理人员",
+        "detail": []
+      },
+      {
+        "id": "hr",
+        "banner": "/imgs/swiper/swiper-03.jpg",
+        "cate": "人事部",
+        "detail": []
+      },
+      {
+        "id": "sale-club",
+        "banner": "/imgs/swiper/swiper-03.jpg",
+        "cate": "销售部",
+        "detail": []
+      },
+      {
+        "id": "study-club",
+        "banner": "/imgs/swiper/swiper-03.jpg",
+        "cate": "研发部",
+        "detail": []
+      },
+      {
+        "id": "newWorker",
+        "banner": "/imgs/swiper/swiper-03.jpg",
+        "cate": "新员工",
+        "detail": [{
+        }]
+      },
+      {
+        "id": "others",
+        "banner": "/imgs/swiper/swiper-03.jpg",
+        "cate": "其他",
+        "detail": [{
+        }]
+      },
+    ],
+    curIndex: 0,
+    isScroll: false,
+    toView: 'exc'
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onReady() {
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  switchTab(e) {
+    const self = this;
+    this.setData({
+      isScroll: true
+    })
+    setTimeout(function () {
+      self.setData({
+        toView: e.target.dataset.id,
+        curIndex: e.target.dataset.index
+      })
+    }, 0)
+    setTimeout(function () {
+      self.setData({
+        isScroll: false
+      })
+    }, 1)
 
   }
+
 })
