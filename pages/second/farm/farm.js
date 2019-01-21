67eb61198ed6aa46bd3271fc776980a39c5b00c9
SongOf <46475785+SongOf@users.noreply.github.com>
2019-01-21 16:37:45 +0800

    Update farm.js

--- pages/second/farm/farm.js
+++ pages/second/farm/farm.js
@@ -1,66 +1,101 @@
 // pages/second/farm/farm.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    category: [
+      { name: '温度', id: 'temperature' },
+      { name: '湿度', id: 'wet' },
+      { name: '虫害', id: 'warm' },
+      { name: '光照', id: 'light' },
+      { name: '其他', id: 'others' }
+    ],
+    detail: [
+      {
+        "id": "temperature",
+        "banner": "/imgs/swiper/swiper-03.jpg",
+        "cate": "温度",
+        "detail": [{
+          "thumb": "/imgs/static/tem.jpg",
+          "name": "东二区"
+        },
+          {
+            "thumb": "/imgs/static/tem.jpg",
+            "name": "南一区"
+          },
+          {
+            "thumb": "/imgs/static/tem.jpg",
+            "name": "北一区"
+          }]
+      },
+      {
+        "id": "wet",
+        "banner": "/imgs/swiper/swiper-03.jpg",
+        "cate": "湿度",
+        "detail": [{
+          "thumb": "/imgs/static/wet.jpg",
+          "name": "西二区"
+        },
+          {
+            "thumb": "/imgs/static/wet.jpg",
+            "name": "东一区"
+          }]
+      },
+      {
+        "id": "warm",
+        "banner": "/imgs/swiper/swiper-03.jpg",
+        "cate": "虫害",
+        "detail": [{
+          "thumb": "/imgs/static/bug.jpg",
+          "name": "室内一区"
+        },
+          {
+            "thumb": "/imgs/static/bug.jpg",
+            "name": "室内二区"
+          },
+          {
+            "thumb": "/imgs/static/bug.jpg",
+            "name": "室外一区"
+          }]
+      },
+      {
+        "id": "light",
+        "banner": "/imgs/swiper/swiper-03.jpg",
+        "cate": "光照",
+        "detail": [{
+          "thumb": "/imgs/test/lightSensor.png",
+          "name": "室内一区"
+        }]
+      },
+      {
+        "id": "others",
+        "banner": "/imgs/swiper/swiper-03.jpg",
+        "cate": "其他",
+        "detail": [{
+        }]
+      },
+    ],
+    curIndex: 0,
+    isScroll: false,
+    toView: 'carGuard'
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onReady() {
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  switchTab(e) {
+    const self = this;
+    this.setData({
+      isScroll: true
+    })
+    setTimeout(function () {
+      self.setData({
+        toView: e.target.dataset.id,
+        curIndex: e.target.dataset.index
+      })
+    }, 0)
+    setTimeout(function () {
+      self.setData({
+        isScroll: false
+      })
+    }, 1)
 
   }
+
 })
